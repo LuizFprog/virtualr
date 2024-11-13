546eae310d4ecf8d5bb85dc418efb606ffd8e2f2
LuizFprog <luizgomes.prog@gmail.com>
2024-11-13 09:03:04 -0300

    feat: HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/NavBar'
+import HeroSection from './components/HeroSection'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   )
 }
